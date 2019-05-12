86b63084aa65f0616d5b015f8fa3832c8d12068f
Nnabugwu Chidozie Gabriel <nnabugwuchidozie@gmail.com>
2019-05-12 10:20:22 +0100

    feature(sign-in):create sign in page
    [STARTS #165907475]

---
+++ signin.js
@@ -0,0 +1,22 @@
+import FormHandler from "./formhandler.js";
+import { Users, UserSession } from "./storage.js";
+const users = new Users();
+const session = new UserSession();
+const ERROR_SELECTOR = '[data-error="text"]';
+const error_sel = document.querySelector(ERROR_SELECTOR);
+const FORM_SELECTOR = '[data-form="form"]';
+const form = document.querySelector(FORM_SELECTOR);
+const formHandler = new FormHandler(form);
+formHandler.addSubmitHandler(data => {
+  console.log(isRegistered(data));
+  if (!isRegistered(data)) return (error_sel.textContent = "* user not registered!");
+  if (!data.email) return (error_sel.textContent = "* enter email");
+  if (!data.password) return (error_sel.textContent = "* enter password");
+  session.set(data.email);
+  location.replace("./index.html");
+});
+
+const isRegistered = data => {
+  const user = users.getUser(data.email);
+  return user && user.password === data.password;
+};
