4653d4c2c82db259691809b756c7784dfd39f3d9
Nnabugwu Chidozie Gabriel <nnabugwuchidozie@gmail.com>
2019-05-13 03:32:02 +0100

    feature(order-car):store order details to database
    [FINISHES #165907487]

---
+++ order.js
@@ -0,0 +1,44 @@
+import FormHandler from './formhandler.js';
+import { UserSession, Table } from './storage.js';
+const session = new UserSession();
+const users = new Table('users');
+const user = users.getUser(session.get());
+const carTable = new Table('cars');
+const cars = Object.values(carTable.getAllFromTable());
+const car = cars[cars.length - 1];
+const orders = new Table('orders');
+const nameSel = document.querySelector('[data-owner="name"]');
+const emailSel = document.querySelector('[data-owner="email"]');
+const modelSel = document.querySelector('[data-car="model"]');
+const makeSel = document.querySelector('[data-car="make"]');
+const yearSel = document.querySelector('[data-car="year"]');
+const colorSel = document.querySelector('[data-car="color"]');
+const bodySel = document.querySelector('[data-car="bodytype"]');
+const mileageSel = document.querySelector('[data-car="mileage"]');
+const descriptionSel = document.querySelector('[data-car="description"]');
+const priceSel = document.querySelector('[data-car="price"]');
+const imgSel = document.querySelector('[data-car="image"]');
+const form = document.querySelector('[data-form="form"]');
+const bidSel = document.querySelector('[data-form="bidprice"]');
+const formHandler = new FormHandler(form);
+const format = num => (+num).toFixed(2).replace(/(\d)(?=(\d{3})+(?!(\d)))/g, '$1 ');
+
+nameSel.textContent = user.firstname + ' ' + user.lastname;
+emailSel.textContent = user.email;
+imgSel.src = car.image;
+makeSel.textContent = car.make;
+modelSel.textContent = car.model;
+yearSel.textContent = car.year;
+colorSel.textContent = car.color;
+bodySel.textContent = car.bodytype || '-';
+mileageSel.textContent = (car.mileage && car.mileage + ' km') || '-';
+descriptionSel.textContent = car.description || '-';
+priceSel.textContent = '₦ ' + format(car.price);
+bidSel.value = car.price;
+
+formHandler.addSubmitHandler(data => {
+  data.carId = car.id;
+  data.userId = user.id;
+  orders.addToTable(data);
+  setTimeout(() => (location.href = './index.html'));
+});
